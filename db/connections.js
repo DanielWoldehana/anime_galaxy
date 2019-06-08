8713a3a8d2007d79c5a24f304a57ae95da30c259
Daniel Woldehana <daniel.woldehana@yahoo.comM>
2019-06-08 17:15:20 -0400

    about to deploy

--- db/connections.js
+++ db/connections.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-if (process.env.NODE_ENV == "production") {
+if (process.env.NODE_ENV === "production") {
   mongoose.connect(process.env.DB_URL);
 } else {
   mongoose.connect("mongodb://localhost/favAnimeDB", {
@@ -11,5 +11,3 @@ if (process.env.NODE_ENV == "production") {
 mongoose.Promise = Promise;
 
 module.exports = mongoose;
-
-heroku config:set DB_URL="mongodb+srv://danielwoldehana:'ilovemongodb22'@cluster0-z4o0m.mongodb.net/test?retryWrites=true&w=majority"
