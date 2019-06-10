dd5f0f6e610d61accbb0129fb358602b69439d31
Daniel Woldehana <daniel.woldehana@yahoo.comM>
2019-06-10 09:48:04 -0400

    added new card

--- fav-anime-react/src/components/HomePage.js
+++ fav-anime-react/src/components/HomePage.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Route, Link, Switch } from "react-router-dom";
 import { Redirect } from "react-router";
 import AnimeDetail from "./AnimeDetail";
-
+import icon from "../images/menu.png";
 class Home extends Component {
   constructor(props) {
     super(props);
@@ -15,25 +15,29 @@ class Home extends Component {
     let showAnime = this.props.allAnime.map((anime, index) => {
       return (
         <div key={anime.mal_id}>
-          <div className="row">
-            <div className="col s12 m6">
-              <div className="card">
-                <div className="card-image">
-                  <img src={anime.image_url} alt={anime.title} />
-                </div>
-                <div className="card-content">
-                  <Link to="/animeDetail">
-                    <span className="card-title">{anime.title}</span>
-                  </Link>
-                  <p>{anime.synopsis}</p>
-                  <Link
-                    to="favAnime"
-                    className="btn-floating halfway-fab waves-effect waves-light red"
-                  >
-                    <i className="material-icons">+</i>
-                  </Link>
-                </div>
-              </div>
+          <div className="card">
+            <div className="card-image waves-effect waves-block waves-light">
+              <img
+                className="activator"
+                src={anime.image_url}
+                alt={anime.title}
+              />
+            </div>
+            <div className="card-content">
+              <span className="card-title activator grey-text text-darken-4">
+                <Link to="/animeDetail">{anime.title}</Link>
+                <i className="material-icons right">more_vert</i>
+              </span>
+              <p>
+                <a href={anime.url}>More info..</a>
+              </p>
+            </div>
+            <div className="card-reveal">
+              <span className="card-title grey-text text-darken-4">
+                {anime.title}
+                <i className="material-icons right">close</i>
+              </span>
+              <p>{anime.synopsis}</p>
             </div>
           </div>
         </div>
